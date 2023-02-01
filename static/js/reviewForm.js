22e3d5f434659acd6945e67eec31e32f386b1a13
Axel Asp <solidtop.asp@gmail.com>
2023-02-01 17:12:47 +0100

    Cleanup

--- static/js/reviewForm.js
+++
@@ -1,9 +0,0 @@
-export default function handleReviewForm() {
-    const form = document.querySelector('.review-form');
-    form.addEventListener('submit', e => {
-        e.preventDefault();
-
-        //Send form to server using fetch here
-    });
-}
-
