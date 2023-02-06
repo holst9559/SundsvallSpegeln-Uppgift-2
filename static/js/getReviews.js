8052d573e16f37a2625097703cc73f496401bd56
yvette809 <60625477+yvette809@users.noreply.github.com>
2023-02-06 07:12:20 +0100

    reviews section styled

--- static/js/getReviews.js
+++ static/js/getReviews.js
@@ -13,13 +13,35 @@ function displayButtons(container, pages, activeIndex) {
   let btns = pages.map((_, pageIndex) => {
     return `<button class = "page-btn ${
       activeIndex === pageIndex ? "active-btn" : "null"
-    }" data-index = '${pageIndex}'>${pageIndex + 1}</button>`;
+    }" data-index = "${pageIndex}">${pageIndex + 1}</button>`;
   });
   btns.push('<button class="next-btn">next</button>');
   btns.unshift('<button class="prev-btn">prev</button>');
 
   container.innerHTML = btns.join("");
 }
+
+btnContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("reviews-btn")) return;
+  if (e.target.classList.contains("page-btn")) {
+    index = parseInt(e.target.dataset.index);
+  }
+  if (e.target.classList.contains("next-btn")) {
+    index++;
+    if (index > pages.length - 1) {
+      index = 0;
+    }
+  }
+
+  if (e.target.classList.contains("prev-btn")) {
+    index--;
+    console.log("previndex", index);
+    if (index < 0) {
+      index = pages;
+    }
+  }
+  setUpUi();
+});
 
 export default async function getReviews() {
   const currentPath = window.location.pathname;
@@ -35,8 +57,12 @@ export default async function getReviews() {
 
 function showReviewsOnDom(reviews) {
   reviews.map((review) => {
+    let list = document.createElement("li");
     let comment = document.createElement("p");
     comment.innerText = review.attributes.comment;
+
+    let authorDiv = document.createElement("div");
+    authorDiv.classList.add("author");
 
     let author = document.createElement("small");
     author.innerText = review.attributes.author;
@@ -59,8 +85,9 @@ function showReviewsOnDom(reviews) {
 
       rating.append(star);
     }
-
-    reviewsDiv.append(comment, author, rating);
+    authorDiv.append(author, rating);
+    list.append(authorDiv, comment);
+    reviewsDiv.appendChild(list);
   });
 }
 
