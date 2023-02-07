c441a88f07867754a4a45ff10527af0137053d78
Anton Holst <holst9559@hotmail.com>
2023-02-07 13:32:53 +0100

    Refactored screeningFilters for easier use in test

--- src/serverFilters/screeningsFilter.js
+++ src/serverFilters/screeningsFilter.js
@@ -1,35 +1,31 @@
-export async function screeningsFilter(res, end_time, items) {
+export default async function screeningsFilter(res, end_time, items) {
   const content = await res.json();
-  const endTime = end_time - 1;
-  const item = items - 1;
+  const endTime = parseInt(end_time);
+  const item = parseInt(items);
   let data = content.data;
+
   let screeningArray = [];
   let filteredArray = [];
   let shortArray = [];
 
-  const todaysDate = new Date().toISOString();
+  const todaysDate = Date.parse(new Date());
+
+  //Setting the date for endTime
   const fiveDays = new Date();
-
   fiveDays.setDate(new Date().getDate() + endTime);
-
-  const fiveDaysFilter = fiveDays.toISOString();
-
-  const fiveDate = fiveDaysFilter.split("T")[0];
+  const fiveDaysParse = Date.parse(fiveDays);
 
   for (let i = 0; i < data.length; i++) {
     screeningArray.push(data[i]);
   }
 
   filteredArray = screeningArray.filter((data) => {
-    const fiveData = data.attributes.start_time.split("T")[0];
-    return (
-      data.attributes.start_time > todaysDate &&
-      fiveData <= fiveDate
-    );
+    const fiveData = Date.parse(data.attributes.start_time);
+    return fiveData > todaysDate && fiveData <= fiveDaysParse;
   });
 
   for (let j = 0; j < filteredArray.length; j++) {
-    if (shortArray.length <= item) {
+    if (shortArray.length < item) {
       shortArray.push(filteredArray[j]);
     } else {
       break;
