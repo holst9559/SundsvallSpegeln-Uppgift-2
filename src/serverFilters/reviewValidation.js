19fc64cc720837c500913aad50307e4e12149e2b
Axel Asp <solidtop.asp@gmail.com>
2023-02-07 15:06:54 +0100

    Include more validation checks

--- src/serverFilters/reviewValidation.js
+++ src/serverFilters/reviewValidation.js
@@ -1,7 +1,6 @@
 export default function validateReview(review) {
     const MIN_NAME_LENGTH = 3;
     const MAX_COMMENT_LENGTH = 200;
-    console.log(review.author.length);
 
     const rating = parseInt(review.rating);
     if (isNaN(rating) || rating < 0 || rating > 5) {
@@ -12,11 +11,27 @@ export default function validateReview(review) {
         };
     }
 
+    if (typeof review.author !== "string") {
+        return {
+            isValid: false,
+            code: 400,
+            message: `Inte en sträng`
+        }
+    } 
+
     if (review.author.length < MIN_NAME_LENGTH) {
         return {
             isValid: false,
             code: 403,
             message: `Namn måste vara minst ${MIN_NAME_LENGTH} karaktärer lång`
+        }
+    }
+
+    if (typeof review.comment !== "string") {
+        return {
+            isValid: false,
+            code: 400,
+            message: `Inte en sträng`
         }
     }
 
